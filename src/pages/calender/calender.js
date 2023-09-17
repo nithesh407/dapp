9216a7f5158acf00a1809d2efd568bd3a17d5b43
LernMaster <pradeept.21cse@kongu.edu>
2023-09-17 21:09:07 +0530

    calender and Notification

--- src/pages/calender/calender.js
+++ src/pages/calender/calender.js
@@ -1,6 +1,7 @@
-import React from 'react';
-import { Badge, Calendar } from 'antd';
+import React, { useState } from 'react';
+import { Badge, Calendar, Modal ,Button,Input,Radio, List} from 'antd';
 import { Navbar } from '../../component';
+import {UnorderedListOutlined} from '@ant-design/icons';
 const getListData = (value) => {
   let listData;
   switch (value.date()) {
@@ -8,11 +9,11 @@ const getListData = (value) => {
       listData = [
         {
           type: 'warning',
-          content: 'This is warning event.',
+          content: 'This is warning event for 8th day.',
         },
         {
           type: 'success',
-          content: 'This is usual event.',
+          content: 'This is usual event for 8th day.',
         },
       ];
       break;
@@ -20,15 +21,15 @@ const getListData = (value) => {
       listData = [
         {
           type: 'warning',
-          content: 'This is warning event.',
+          content: 'This is warning event for 10th day.',
         },
         {
           type: 'success',
-          content: 'This is usual event.',
+          content: 'This is usual event for 10th day.',
         },
         {
           type: 'error',
-          content: 'This is error event.',
+          content: 'This is error event for 10th day.',
         },
       ];
       break;
@@ -36,75 +37,136 @@ const getListData = (value) => {
       listData = [
         {
           type: 'warning',
-          content: 'This is warning event',
+          content: 'This is warning event for 15th day',
         },
         {
           type: 'success',
-          content: 'This is very long usual event......',
+          content: 'This is very long usual event for 15th day......',
         },
         {
           type: 'error',
-          content: 'This is error event 1.',
+          content: 'This is error event 1 for 15th day.',
         },
         {
           type: 'error',
-          content: 'This is error event 2.',
+          content: 'This is error event 2 for 15th day.',
         },
         {
           type: 'error',
-          content: 'This is error event 3.',
+          content: 'This is error event 3 for 15th day.',
         },
         {
           type: 'error',
-          content: 'This is error event 4.',
+          content: 'This is error event 4 for 15th day.',
         },
       ];
       break;
     default:
+      // Handle other days if needed
+      break;
   }
   return listData || [];
 };
-const getMonthData = (value) => {
-  if (value.month() === 8) {
-    return 1394;
-  }
-};
+
 const Nfcalendar = () => {
-  const monthCellRender = (value) => {
-    const num = getMonthData(value);
-    return num ? (
-      <div className="notes-month">
-        <section>{num}</section>
-        <span>Backlog number</span>
-      </div>
-    ) : null;
+  const [modalVisible, setModalVisible] = useState(false);
+  const [nestedModalVisible, setNestedModalVisible] = useState(false);
+  const [selectedDate, setSelectedDate] = useState(null);
+
+  const handleDateClick = (value) => {
+    setSelectedDate(value);
+    setModalVisible(true);
   };
+
+  const closeModal = () => {
+    setModalVisible(false);
+  };
+
+  const handleNestedModalClick = () => {
+    setNestedModalVisible(true);
+  };
+
+  const closeNestedModal = () => {
+    setNestedModalVisible(false);
+  };
+  function onChange(e) {
+    console.log(`radio checked:${e.target.value}`);
+  }
   const dateCellRender = (value) => {
-    const listData = getListData(value);
     return (
-      <ul className="events">
-        {listData.map((item) => (
-          <li key={item.content}>
-            <Badge status={item.type} text={item.content} />
-          </li>
+      <div onClick={() => handleDateClick(value)} className="date-cell" style={{ height: '70px' }}>
+        {getListData(value).map((item, index) => (
+          <Badge key={index} status={item.type} text={item.content} />
         ))}
-      </ul>
+      </div>
     );
   };
-  const cellRender = (current, info) => {
-    if (info.type === 'date') return dateCellRender(current);
-    if (info.type === 'month') return monthCellRender(current);
-    return info.originNode;
-  };
-  return(
-    <>
-    
-    <Navbar/>
-    <div style={{ width: '75%', margin: '0 auto' }}>
-  <Calendar cellRender={cellRender} style={{border:'2px solid black'}}/>
-</div>
 
-  </>
+  return (
+    <>
+      <Navbar />
+      <div style={{ width: '75%', margin: '0 auto' }}>
+        <Calendar dateCellRender={dateCellRender} />
+      </div>
+      <Modal
+        title={`Events for ${selectedDate ? selectedDate.format('YYYY-MM-DD') : ''}`}
+        open={modalVisible}
+        onCancel={closeModal}
+        footer={[
+          <Button key="nestedModalButton" onClick={handleNestedModalClick}>
+            Open Nested Modal
+          </Button>,
+          <Button key="close" onClick={closeModal}>
+            Close
+          </Button>,
+        ]}
+      >
+        {selectedDate && (
+          <div>
+            <p>Date: {selectedDate.format('YYYY-MM-DD')}</p>
+            <List className="events">
+              {getListData(selectedDate).map((item, index) => (
+                <List.Item key={index}>
+                  <Badge status={item.type} text={item.content} />
+                  <Button type="danger" style={{backgroundColor:'#ff4d4f',color:'#ffffff'}}>Remove</Button>
+                </List.Item>
+              ))}
+            </List>
+          </div>
+        )}
+      </Modal>
+      <Modal
+        title="Nested Modal"
+        centered
+        open={nestedModalVisible}
+        onCancel={closeNestedModal}
+        footer={[
+          <Button key="closeNested" onClick={closeNestedModal}>
+            Close
+          </Button>,
+          <Button key="submit" type='primary'>
+          Submit
+        </Button>
+        ]
+      }
+      >
+        <Input
+      placeholder="Enter the event"
+      prefix={<UnorderedListOutlined />}
+        />
+        <br/>
+        <br/>
+        <div>
+      <Radio.Group onChange={onChange} defaultValue="success">
+        <Radio.Button value="success">Low</Radio.Button>
+        <Radio.Button value="warning">Intermediate</Radio.Button>
+        <Radio.Button value="error">High</Radio.Button>
+      </Radio.Group>
+    </div>
+      </Modal>
+    </>
   );
 };
+
+
 export default Nfcalendar;
--- src/userDashboard/client/ClientDashboard.js
+++ src/userDashboard/client/ClientDashboard.js
@@ -1,35 +1,9 @@
 import React, { useEffect } from 'react';
-import { notification } from 'antd';
 import { Navbar } from '../../component';
-import { useNavigate } from 'react-router-dom';
 
 const ClientDashboard = () => {
-  const [api, contextHolder] = notification.useNotification();
-  const navigate = useNavigate();
-
-  const openNotification = () => {
-    const key = 'custom_notification';
-
-    const onClick = () => {
-      navigate('/notifications');
-    };
-
-    api.open({
-      message: 'Notification Title',
-      description:
-        'I will never close automatically. This is a purposely very very long description that has many many characters and words.',
-      duration: 2,
-      key: key,
-      onClick: onClick,
-    });
-  };
-
-  useEffect(() => {
-    openNotification();
-  }, []);
   return (
     <>
-      {contextHolder}
       <Navbar />
     </>
   );
