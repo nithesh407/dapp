84f056ece97b404021d7bb75d5e594bb23c3ec97
LernMaster <pradeept.21cse@kongu.edu>
2023-09-17 21:09:07 +0530

    calender and Notification

--- src/component/navbar/navbar.js
+++ src/component/navbar/navbar.js
@@ -1,7 +1,7 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { DashboardOutlined, FileTextOutlined, CalendarOutlined, SettingOutlined, BellOutlined, UserOutlined, TeamOutlined } from '@ant-design/icons';
-import { Menu, Layout } from 'antd';
-import { useLocation } from 'react-router-dom'; // Import useLocation
+import { Menu, Layout, Drawer, notification,List,Avatar} from 'antd';
+import { useLocation } from 'react-router-dom';
 import styles from './navbar.module.css';
 import logo from '../../assets/profile.png';
 
@@ -34,29 +34,51 @@ const leftItems = [
     icon: <TeamOutlined />,
   },
 ];
-
-const rightItems = [
+const data = [
   {
-    label: 'Notifications',
-    key: 'notifications',
-    icon: <BellOutlined />,
+    title: 'Ant Design Title 1',
   },
+  {
+    title: 'Ant Design Title 2',
+  },
+  {
+    title: 'Ant Design Title 3',
+  },
+  {
+    title: 'Ant Design Title 4',
+  },
+  {
+    title: 'Ant Design Title 5',
+  },
+  {
+    title: 'Ant Design Title 6',
+  },
+  {
+    title: 'Ant Design Title 7',
+  },
+  {
+    title: 'Ant Design Title 8',
+  },
+];
+const rightItems = [
   {
     label: 'Settings',
     key: 'settings',
     icon: <SettingOutlined />,
   },
   {
-    key:'user',
+    key: 'user',
     icon: <img src={logo} height={30} width={30} style={{ marginTop: '5px', marginLeft: '10px' }} />,
   },
 ];
 
 const App = () => {
-  const location = useLocation(); // Get the current location (URL)
+  const location = useLocation();
   const [current, setCurrent] = useState(getCurrentKeyFromLocation(location));
+  const [drawerVisible, setDrawerVisible] = useState(false);
 
-  // Function to extract the key from the location pathname
+  const [api, contextHolder] = notification.useNotification();
+
   function getCurrentKeyFromLocation(location) {
     const path = location.pathname;
     for (const item of leftItems.concat(rightItems)) {
@@ -64,13 +86,47 @@ const App = () => {
         return item.key;
       }
     }
-    return 'dapp'; // Default key if no match is found
+    return 'dapp';
   }
 
   const onClick = (e) => {
-    console.log('click ', e);
     setCurrent(e.key);
   };
+
+  const showDrawer = () => {
+    setDrawerVisible(true);
+  };
+
+  const onCloseDrawer = () => {
+    setDrawerVisible(false);
+  };
+
+  const handleNotificationClick = () => {
+    showDrawer();
+    api.destroy('custom_notification');
+  };
+
+  useEffect(() => {
+    const openNotification = () => {
+      const key = 'custom_notification';
+
+      const onClick = () => {
+        showDrawer();
+        api.destroy(key);
+      };
+
+      api.open({
+        message: 'Notification Title',
+        description:
+          'I will never close automatically. This is a purposely very very long description that has many many characters and words.',
+        duration: 5,
+        key: key,
+        onClick: onClick,
+      });
+    };
+
+    openNotification();
+  }, [api]);
 
   return (
     <div className={styles['menu-container']} >
@@ -82,12 +138,34 @@ const App = () => {
         ))}
       </Menu>
       <Menu style={{ minWidth: 350 }} onClick={onClick} selectedKeys={[current]} mode="horizontal">
+        <Menu.Item onClick={handleNotificationClick} icon={<BellOutlined/>}>Notification</Menu.Item>
         {rightItems.map((item) => (
           <Menu.Item key={item.key} icon={item.icon}>
             <a href={'/' + item.key}>{item.label}</a>
           </Menu.Item>
         ))}
       </Menu>
+      <Drawer
+        title="Drawer Title"
+        placement="right"
+        onClose={onCloseDrawer}
+        visible={drawerVisible}
+      >
+        <List
+    itemLayout="horizontal"
+    dataSource={data}
+    renderItem={item => (
+      <List.Item>
+        <List.Item.Meta
+          avatar={<Avatar src={logo} />}
+          title={<a href="https://ant.design">{item.title}</a>}
+          description="Ant Design, a design language for background applications, is refined by Ant UED Team"
+        />
+      </List.Item>
+    )}
+  />
+      </Drawer>
+      {contextHolder}
     </div>
   );
 };
