21136e580342e2d3c68a1a40fe762babc60cbf93
LernMaster <pradeept.21cse@kongu.edu>
2023-09-21 20:55:38 +0530

    configured ipfs with document

---
+++ src/pages/documents/DocumentModal.js
@@ -0,0 +1,170 @@
+import React, { useState } from "react";
+import { Modal, Input, Select, DatePicker, Upload, Button, message } from "antd";
+import { PlusOutlined, UploadOutlined } from "@ant-design/icons";
+import { create } from 'ipfs-http-client';
+import FileDownload from "./FileDownload";
+
+const { Option } = Select;
+
+const DocumentModal = ({ children, onFormData }) => {
+  const ipfs = create({ host: 'localhost', port: 5001, protocol: 'http' });
+  const [visible, setVisible] = useState(false);
+  const [fileList, setFileList] = useState([]);
+  const [caseName, setCaseName] = useState(''); // Store Case Name
+  const [caseNumber, setCaseNumber] = useState(''); // Store Case Number
+  const [tags, setTags] = useState([]); // Store Tags as an array
+  const [date, setDate] = useState('');
+  const [cid, setCID] = useState('');
+
+  const handleCancel = () => {
+    setVisible(false);
+  };
+
+  const handleUpload = (cid) => {
+    // Handle the upload logic here
+    console.log('File uploaded with CID:', cid);
+    message.success('File uploaded successfully');
+    // Clear the form fields after upload
+    setCaseName('');
+    setCaseNumber('');
+    setTags([]);
+    setFileList([]);
+    setDate(null);
+  };
+
+  const showModal = () => {
+    setVisible(true);
+  };
+
+  const handleFileChange = ({ fileList }) => {
+    setFileList(fileList);
+  };
+
+  function handleChange(value) {
+    setTags(value); // Update the tags state
+  }
+
+  const uploadFile = async () => {
+    if (fileList.length === 0) {
+      alert('Please select files to upload.');
+      return;
+    }
+
+    try {
+      // Loop through the fileList array to process each selected file
+      for (const fileItem of fileList) {
+        const file = fileItem.originFileObj;
+        const fileContent = await file.arrayBuffer();
+        const content = new Uint8Array(fileContent);
+        const result = await ipfs.add({ content });
+
+        setCID(result.cid.toString());
+        handleUpload(result.cid.toString());
+      }
+
+      // Clear the fileList after uploading
+      setFileList([]);
+    } catch (error) {
+      console.error('Error uploading files to IPFS:', error);
+    }
+  };
+
+  const handleOk = () => {
+    const formData = {
+      caseName,
+      caseNumber,
+      tags,
+      date,
+      cid,
+    };
+    console.log(formData);
+    // Call the onFormData prop with the form data
+    onFormData(formData);
+
+    // Clear the form fields and close the modal
+    setCaseName('');
+    setCaseNumber('');
+    setTags([]);
+    setFileList([]);
+    setDate(null);
+    setVisible(false);
+  };
+
+  return (
+    <>
+      <Button type="primary" onClick={showModal}>
+        New <PlusOutlined />
+      </Button>
+      <Modal
+        title="Basic Modal"
+        visible={visible}
+        onOk={handleOk}
+        onCancel={handleCancel}
+      >
+        <Input
+          style={{ marginTop: 10 }}
+          placeholder="Case Name"
+          value={caseName}
+          onChange={(e) => setCaseName(e.target.value)}
+        />
+        <Input
+          style={{ marginTop: 10 }}
+          placeholder="Enter Case Number"
+          value={caseNumber}
+          onChange={(e) => setCaseNumber(e.target.value)}
+        />
+        <Select
+          style={{ marginTop: 10, width: '100%' }}
+          mode="tags"
+          placeholder="Tags Mode"
+          onChange={handleChange}
+          value={tags}
+        >
+          {children}
+        </Select>
+        <Input
+          type="date"
+          style={{ width: '100%', marginTop: 10 }}
+          value={date}
+          onChange={(e) => setDate(e.target.value)}
+        />
+        <Upload
+          style={{ marginTop: 10 }}
+          customRequest={() => { }}
+          onChange={handleFileChange}
+          fileList={fileList}
+          showUploadList={{ showPreviewIcon: true, showRemoveIcon: true }}
+        >
+          <Button icon={<UploadOutlined />} style={{ marginTop: 10 }}>
+            Select File(s)
+          </Button>
+        </Upload>
+
+        <Button
+          type="primary"
+          style={{ marginTop: 10 }}
+          onClick={uploadFile}
+        >
+          Upload
+        </Button>
+
+        {cid && (
+          <div>
+            <p>Document CID: {cid}</p>
+            <a
+              href={`https://ipfs.io/ipfs/${cid}`}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              View on IPFS
+            </a>
+
+            <FileDownload cid={cid} />
+          </div>
+        )}
+      </Modal>
+    </>
+  );
+};
+
+export default DocumentModal;
--- src/pages/documents/lawyerDocument.js
+++ src/pages/documents/lawyerDocument.js
@@ -1,11 +1,67 @@
-import React,{useState} from "react";
+import React, { useState, useEffect } from "react";
 import { Navbar } from "../../component";
-import { Button, Menu, Dropdown,Avatar, Card ,Modal,Input,Select,DatePicker,message,Upload} from 'antd';
-import {DownOutlined,PlusOutlined,UploadOutlined,EditOutlined, SettingOutlined,SearchOutlined } from '@ant-design/icons'
+import { Button, Menu, Dropdown, Avatar, Card, Select, Tag } from 'antd';
+import { DownOutlined, PlusOutlined, UploadOutlined, EditOutlined, SettingOutlined } from '@ant-design/icons'
 import folder from "../../assets/folder.png"
+import DocumentModal from "./DocumentModal";
 const { Meta } = Card;
 const { Option } = Select;
-const LawyerDocument =()=>{
+
+const initialData = [
+  {
+    caseNumber: 'Case 1',
+    startDate: '2023-09-21',
+    tags: ['Tag1', 'Tag2'],
+    title: 'Card title 1',
+    imageUrl: 'https://xsgames.co/randomusers/avatar.php?g=pixel',
+  },
+  {
+    caseNumber: 'Case 2',
+    startDate: '2023-09-22',
+    tags: ['Tag3', 'Tag4'],
+    title: 'Card title 2',
+    imageUrl: 'https://xsgames.co/randomusers/avatar.php?g=pixel',
+  },
+];
+
+const LawyerDocument = () => {
+  const [data, setData] = useState(initialData);
+
+  const renderCards = () => {
+    return (
+      <div style={{ display: 'grid', margin: '30px', gridTemplateColumns: 'repeat(auto-fill, minmax(300px, 1fr))', gap: '20px' }}>
+        {data.slice().reverse().map((cardData, index) => (
+          <div key={index}>
+            <Card
+              style={{
+                border: '1px solid lightgrey',
+              }}
+              cover={<img alt="example" src={folder} />}
+              actions={[
+                <SettingOutlined key="setting" />,
+                <EditOutlined key="edit" />,
+                <UploadOutlined key="upload" />,
+              ]}
+            >
+              <Meta
+                avatar={<Avatar src={cardData.imageUrl} />}
+                title={cardData.title}
+                description={
+                  <div>
+                    Case Number: {cardData.caseNumber}<br />
+                    Start Date: {cardData.startDate}<br />
+                    {cardData.tags.map((tag, index) => (<Tag key={index}>{tag}</Tag>))}
+                  </div>
+                }
+              />
+            </Card>
+          </div>
+        ))}
+      </div>
+    );
+  };
+
+
   const caseTypes = [
     { prefix: "CR", name: "Criminal Cases" },
     { prefix: "CV", name: "Civil Cases" },
@@ -28,309 +84,51 @@ const LawyerDocument =()=>{
     { prefix: "IN", name: "International Cases" },
     // Add more case types here with prefixes
   ];
+
   const children = caseTypes.map((caseType) => (
     <Option key={caseType.prefix}>{`${caseType.prefix} - ${caseType.name}`}</Option>
   ));
-
-  function handleChange(value) {
-    console.log(`selected ${value}`);
+  const handleFormData = (formData) => {
+    const newElement = {
+      caseNumber: formData.caseNumber,
+      startDate: formData.date,
+      tags: formData.tags,
+      title: formData.caseName,
+      imageUrl: 'https://xsgames.co/randomusers/avatar.php?g=pixel',
+    };
+    console.log('Received form data:', formData);
+    setData([...data, newElement]);
+    
+  };
+  function handleMenuClick(e) {
+    console.log('click', e);
   }
-  const [searchQuery, setSearchQuery] = useState("");
-  const [filteredCards, setFilteredCards] = useState([]);
-  const [visible, setVisible] = useState(false);
-  const [, setStartDate] = useState(null);
-  const [settingsVisible, setSettingsVisible] = useState(false);
-  const [deleteConfirmVisible, setDeleteConfirmVisible] = useState(false);
-  // State for controlling the "Edit" modal
-  const [editVisible, setEditVisible] = useState(false);
-  const [description, setDescription] = useState(""); // Description input value
 
-  // State for controlling the "Upload" modal
-  const [uploadVisible, setUploadVisible] = useState(false);
+  const menu = (
+    <Menu onClick={handleMenuClick}>
+      <Menu.Item key="1">a - z</Menu.Item>
+      <Menu.Item key="2">Date</Menu.Item>
+      <Menu.Item key="3">Size</Menu.Item>
+    </Menu>
+  );
 
-  // Function to handle the "Settings" button click
-  const handleSettingsClick = () => {
-    setSettingsVisible(true);
-  };
-
-  // Function to handle the "Edit" button click
-  const handleEditClick = () => {
-    setEditVisible(true);
-  };
-
-  // Function to handle the "Upload" button click
-  const handleUploadClick = () => {
-    setUploadVisible(true);
-  };
-
-  // Function to handle deleting a document
-  const handleDelete = () => {
-    // Implement your delete logic here
-    setSettingsVisible(false);
-    message.success("Document deleted successfully");
-  };
-
-  // Function to handle collaboration
-  const handleCollaborate = () => {
-    // Implement your collaboration logic here
-    setSettingsVisible(false);
-    message.success("Collaboration request sent");
-  };
-
-  // Function to handle saving edited description
-  const handleSaveEdit = () => {
-    // Implement your save edit logic here
-    setEditVisible(false);
-    message.success("Description updated successfully");
-  };
-
-  // Function to handle file upload
-  const handleUploadFiles = () => {
-    // Implement your file upload logic here
-    setUploadVisible(false);
-    message.success("Files uploaded successfully");
-  };
-
-  const showModal = () => {
-    setVisible(true);
-  };
-
-  const handleOk = (e) => {
-    console.log(e);
-    setVisible(false);
-  };
-
-  const handleCancel = (e) => {
-    console.log(e);
-    setVisible(false);
-  };
-
-function handleMenuClick(e) {
-  console.log('click', e);
-}
-const [fileList, setFileList] = useState([]);
-
-const handleFileChange = ({ fileList }) => {
-  // Handle file changes here, such as updating the state with selected files.
-  setFileList(fileList);
-};
-
-const handleUpload = () => {
-  // Handle the file upload logic here, such as sending the files to the server.
-  // You can use the fileList state to access the selected files.
-  console.log('Uploading files:', fileList);
-  // You can implement your upload logic here.
-  message.success('File uploaded successfully');
-};
-const caseData = [
-  {
-    id: 1,
-    caseName: "Arjun",
-    description: "Theft case",
-    avatar: "https://xsgames.co/randomusers/avatar.php?g=pixel"
-  },
-  {
-    id: 2,
-    caseName: "Priya",
-    description: "Land case",
-    avatar: "https://xsgames.co/randomusers/avatar.php?g=pixel"
-  },
-  {
-    id: 3,
-    caseName: "Rajesh",
-    description: "Civil case",
-    avatar: "https://xsgames.co/randomusers/avatar.php?g=pixel"
-  },
-  {
-    id: 4,
-    caseName: "Pooja",
-    description: "Criminal case",
-    avatar: "https://xsgames.co/randomusers/avatar.php?g=pixel"
-  },
-  {
-    id: 5,
-    caseName: "Rahul",
-    description: "Pocso case",
-    avatar: "https://xsgames.co/randomusers/avatar.php?g=pixel"
-  },
-  {
-    id: 6,
-    caseName: "Ananya",
-    description: "Civil case",
-    avatar: "https://xsgames.co/randomusers/avatar.php?g=pixel"
-  },
-  {
-    id: 7,
-    caseName: "Suresh",
-    description: "Criminal case",
-    avatar: "https://xsgames.co/randomusers/avatar.php?g=pixel"
-  },
-  {
-    id: 8,
-    caseName: "Nithya",
-    description: "Civil case",
-    avatar: "https://xsgames.co/randomusers/avatar.php?g=pixel"
-  },
-];
-const filterCases = () => {
-  const filtered = caseData.filter(caseItem => {
-    return caseItem.caseName.toLowerCase().includes(searchQuery.toLowerCase());
-  });
-  setFilteredCards(filtered);
-};
-
-// Call filterCases whenever searchQuery changes
-React.useEffect(() => {
-  filterCases();
-}, [searchQuery]);
-
-const menu = (
-  <Menu onClick={handleMenuClick}>
-    <Menu.Item key="1">a - z</Menu.Item>
-    <Menu.Item key="2">Date</Menu.Item>
-    <Menu.Item key="3">Size</Menu.Item>
-  </Menu>
-);
-
-
-
-
-    return(
+  return (
     <div>
-        <Navbar />
-        <div style={{marginTop:70}}>
-        <div style={{marginLeft:60,width:700}}>
-        <Button  type="primary" onClick={showModal}>New <PlusOutlined></PlusOutlined></Button>
-        <Modal
-        title="Basic Modal"
-        visible={visible}
-        onOk={handleOk}
-        onCancel={handleCancel}
-      >
-        
-        <Input placeholder="Enter Case Number" />
-        <Select mode="tags" style={{ width: '100%' }} placeholder="Tags Mode" onChange={handleChange}>
-              {children}
-            </Select>
-            <DatePicker
-              style={{ width: '100%', marginTop: 10 }}
-              placeholder="Select Start Date"
-              onChange={(date, dateString) => setStartDate(dateString)}
-            />
-
-<Upload
-        customRequest={() => {}}
-        onChange={handleFileChange}
-        fileList={fileList}
-        showUploadList={{ showPreviewIcon: true, showRemoveIcon: true }}
-      >
-        <Button icon={<UploadOutlined />}>Select File(s)</Button>
-      </Upload>
-
-      <Button type="primary" onClick={handleUpload}>
-        Upload
-      </Button>  
-      </Modal>
-
-        <Button style={{marginLeft:10,width:100}}>Filter</Button>
-        <Dropdown style={{marginLeft:10,width:100}} overlay={menu}>
-          <Button>
-            Sort <DownOutlined />
-          </Button>
-        </Dropdown>
-        <Input
-            style={{ marginLeft:1190, width: 200 }}
-            placeholder="Search Case"
-            prefix={<SearchOutlined />}
-            onChange={(e) => setSearchQuery(e.target.value)}
-            
-          />
-
+      <Navbar />
+      <div style={{ marginTop: 70 }}>
+        <div style={{ marginLeft: 60, width: 700 }}>
+          <DocumentModal onFormData={handleFormData}>{children}</DocumentModal>
+          <Button style={{ marginLeft: 10, width: 100 }}>Filter</Button>
+          <Dropdown style={{ marginLeft: 10, width: 100 }} overlay={menu}>
+            <Button>
+              Sort <DownOutlined />
+            </Button>
+          </Dropdown>
         </div>
-        </div>
-        <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'space-between' ,marginLeft:50,marginRight:50,marginTop:50 }}>
-          {filteredCards.map(caseItem => (
-            <div key={caseItem.id} style={{ width: '300px', marginBottom: 20}}>
-              <Card
-                style={{
-                  border: '1px solid lightgrey'
-                }}
-                cover={
-                  <img
-                    alt="example"
-                    src={folder}
-                  />
-                }
-                actions={[
-                    <Button onClick={handleSettingsClick}><SettingOutlined key="setting" /></Button>,
-      <Button onClick={handleEditClick}><EditOutlined key="edit" /></Button>,
-      <Button onClick={handleUploadClick}><UploadOutlined key="edit" /></Button>,
-
-      
-                  ]}
-              >
-                <Meta
-                  avatar={<Avatar src={caseItem.avatar} />}
-                  title={caseItem.caseName}
-                  description={caseItem.description}
-                />
-              </Card>
-            </div>
-          ))}
-       {/* Settings Modal */}
-      <Modal
-        title="Settings"
-        visible={settingsVisible}
-        onCancel={() => setSettingsVisible(false)}
-        footer={[
-          <Button key="delete" type="primary" onClick={handleDelete}>
-            Delete
-          </Button>,
-          <Button key="collaborate" type="primary" onClick={handleCollaborate}>
-            Collaborate
-          </Button>,
-        ]}
-      >
-        {/* Settings modal content */}
-      </Modal>
-
-      {/* Edit Modal */}
-      <Modal
-        title="Edit Description"
-        visible={editVisible}
-        onOk={handleSaveEdit}
-        onCancel={() => setEditVisible(false)}
-      >
-        <Input
-          placeholder="Enter Description"
-          value={description}
-          onChange={(e) => setDescription(e.target.value)}
-        />
-      </Modal>
-
-      {/* Upload Modal */}
-      <Modal
-        title="Upload Files"
-        visible={uploadVisible}
-        onOk={handleUploadFiles}
-        onCancel={() => setUploadVisible(false)}
-      >
-        <Upload
-          customRequest={() => {}}
-          onChange={handleFileChange}
-          fileList={fileList}
-          showUploadList={{ showPreviewIcon: true, showRemoveIcon: true }}
-        >
-          <Button icon={<UploadOutlined />}>Select File(s)</Button>
-        </Upload>
-      </Modal>
-  
-    
-</div>
-        
-</div>
-    
-    )
-}
+        <div>{renderCards()}</div>
+      </div>
+    </div>
+  );
+};
 
 export default LawyerDocument;
